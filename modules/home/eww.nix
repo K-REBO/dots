edb8d9d1f6e5c515a4e2450b153dd0d1475f97fe
Bido Nakamura <K-REBO@users.noreply.github.com>
2026-02-02 05:53:05 +0900

    eww tweaks

--- modules/home/eww.nix
+++ modules/home/eww.nix
@@ -2,10 +2,11 @@
 
 {
   # eww (ElKowars wacky widgets)
-  programs.eww = {
-    enable = true;
-    configDir = ../../config/eww;
-  };
+  programs.eww.enable = true;
+
+  # 設定ファイルをシンボリックリンク（nix store外の実パスを参照）
+  home.file.".config/eww".source =
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nix/config/eww";
 
   # 依存パッケージ
   home.packages = with pkgs; [
