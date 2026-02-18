9cd316cff9fd679b479a7b0d973c5c4d8871de4c
Bido Nakamura <K-REBO@users.noreply.github.com>
2026-02-19 02:30:30 +0900

    config Vicinae

--- modules/home/vicinae.nix
+++ modules/home/vicinae.nix
@@ -3,4 +3,5 @@
   home.packages = with pkgs; [ vicinae ];
 
   xdg.configFile."vicinae/config.jsonc".source = ../../config/vicinae/config.jsonc;
+  xdg.configFile."vicinae/settings.json".source = ../../config/vicinae/settings.json;
 }
